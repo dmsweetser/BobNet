8d5ed410481d078e9d209d6e96e5eeecc9e36903
Daniel Sweetser <dmsweetser@gmail.com>
2024-04-03 13:05:36 -0400

    Changes BobNet to consume any provided file in chunks rather than as a whole

--- bob_net.py
+++ bob_net.py
@@ -2,6 +2,7 @@ import sys
 import os
 import argparse
 from lib.bob import *
+from lib.utilities import *
 import shutil
 from lib.vector_store import VectorStore
 import codecs
@@ -13,7 +14,7 @@ share_dir = "share"
 max_results = 10
 
 config = {
-    "context_length": 256,
+    "context_length": 512,
     "embedding_dim": 64,
     "lstm_units": 64, 
     "hidden_dim": 30000,
@@ -54,18 +55,19 @@ if __name__ == "__main__":
                 continue
             full_file_path = os.path.join(ingest_dir, file)
             with codecs.open(full_file_path, 'rU', encoding='utf-8') as ingest_file:
-                training_text = ingest_file.read()
-                new_bob = Bob(config=config, training_data=training_text)
-                vector_store.add_vector(
-                    training_text,
-                    new_bob.save_bob())
-                
-                if generate_bob_for_sharing:
-                    current_ticks = int(time.time())
-                    file_name = f"{str(current_ticks)}.bob"
-                    full_path = os.path.join(share_dir, file_name)
-                    with open(full_path, "w") as share_file:
-                        share_file.write(json.dumps(new_bob.save_bob(), indent=4))
+                training_text_raw = ingest_file.read()                
+                split_training_text = wrap_text(training_text_raw, config["context_length"])
+                for training_text in split_training_text:                
+                    new_bob = Bob(config=config, training_data=training_text)
+                    vector_store.add_vector(
+                        training_text,
+                        new_bob.save_bob())                    
+                    if generate_bob_for_sharing:
+                        current_ticks = int(time.time())
+                        file_name = f"{str(current_ticks)}.bob"
+                        full_path = os.path.join(share_dir, file_name)
+                        with open(full_path, "w") as share_file:
+                            share_file.write(json.dumps(new_bob.save_bob(), indent=4))
             if archive_ingested_files:
                 shutil.move(full_file_path, os.path.join(ingest_archive_dir,file))
         
---
+++ lib/utilities.py
@@ -0,0 +1,4 @@
+import textwrap
+
+def wrap_text(text, width):
+    return textwrap.wrap(text, width=width)
