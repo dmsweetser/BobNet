936c458bbfbdcc5b8b8c2d5bbcf10acb63898a42
Daniel Sweetser <dmsweetser@gmail.com>
2024-04-03 16:42:38 -0400

    Fixes chunking

--- bob_net.py
+++ bob_net.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
             full_file_path = os.path.join(ingest_dir, file)
             with codecs.open(full_file_path, 'rU', encoding='utf-8') as ingest_file:
                 training_text_raw = ingest_file.read()                
-                split_training_text = wrap_text(training_text_raw, config["context_length"])
+                split_training_text = string_chunks(training_text_raw, config["context_length"])
                 for training_text in split_training_text:                
                     new_bob = Bob(config=config, training_data=training_text)
                     vector_store.add_vector(
--- lib/utilities.py
+++ lib/utilities.py
@@ -1,10 +1,3 @@
-import textwrap
-import math
-
-def wrap_text(text, width):
-    lines = textwrap.wrap(text, width=width)
-    offset = int(math.floor((width / 2) + 0.5))
-    start_indices = [i for i in range(0, len(lines), width)]
-    start_indices = [max(start_indices[i] + width + offset, start_indices[i]) for i in range(len(start_indices))]
-    overlapped_lines = [lines[i][:start_indices[i] - offset] for i in range(len(lines))]
-    return overlapped_lines
+def string_chunks(string, chunk_size):
+    step = chunk_size // 2
+    return [string[i:i + chunk_size] for i in range(0, len(string) - chunk_size, step)]
